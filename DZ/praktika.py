c0dcee88f417828e45a4b0b2497c64484b444b1e
RomanP1605 <roman.pautov1605@gmail.com>
2023-02-05 12:18:35 +0200

    dz11 edited

--- DZ/praktika.py
+++
@@ -1,7 +0,0 @@
-students = [["Роман", "Паутов", "13"], ["Илья", "Іванов", "14"], ["Илья", "Селезень", "14"]]
-abc = 0
-for student in students:
-    print(student)
-    if "Илья" in student:
-        abc += 1
-print("Илья зустрічається стільки разів", abc)
