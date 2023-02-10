cf248228118fc05b56c64583823c2528d3f1a037
RomanP1605 <roman.pautov1605@gmail.com>
2023-02-10 14:12:49 +0200

    dz13-14 added

---
+++ DZ/dz13-14/dz13-14.py
@@ -0,0 +1,13 @@
+owner1 = "Oleg Ivanov"
+owner2 = "Yuriy Oleksandrovich"
+owner3 = "Artem Grigorievich"
+marks1 = " Audi Q7"
+marks2 = " BMW E3"
+marks3 = " Tatra 613"
+input("Введіть номер авто (англійськими буквами!):")
+if "AI2002MO":
+    print(owner1 + marks1)
+elif "AA4536HP":
+    print(owner2 + marks2)
+elif "BH2047KO":
+    print(owner3 + marks3)
