91c5468976b2582e8558d5f817ac1a861a86889c
RomanP1605 <roman.pautov1605@gmail.com>
2023-02-26 13:06:25 +0200

    dz15-16 added

---
+++ DZ/111.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+input_values=['27.02', '28.02', '1.03', '2.03', '3.03']
+t=[9,13,8,20,18]
+fig, ax=plt.subplots()
+ax.plot(t)
+
+plt.show
--- DZ/asterix.py
+++ DZ/asterix.py
@@ -1,4 +1,15 @@
 import turtle
+import random
 
 t=turtle.Turtle()
-for c in ['red', 'purple', ]
+
+for a in range(1,11):
+    t.penup()
+    t.setx(random.randint(-300, 300))
+    t.sety(random.randint(-300, 300))
+    t.pendown()
+
+for c in ['red', 'purple', 'blue', 'black', 'green']:
+    t.color(c)
+    t.forward(200)
+    t.left(144)
