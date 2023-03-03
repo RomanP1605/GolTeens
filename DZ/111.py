cf94b578631ad4500cc817e2eea5876da89c04f9
RomanP1605 <roman.pautov1605@gmail.com>
2023-03-03 18:24:30 +0200

    dz15-16 added

--- DZ/111.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-input_values=['27.02', '28.02', '1.03', '2.03', '3.03']
-t=[9,13,8,20,18]
-fig, ax=plt.subplots()
-ax.plot(t)
-
-plt.show
---
+++ DZ/dz_17.py
@@ -0,0 +1,6 @@
+import turtle
+
+t = turtle.Turtle()
+wn = turtle.Screen()
+wn.setup(500, 500)
+t.circle(100)
